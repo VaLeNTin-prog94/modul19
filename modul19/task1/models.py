c98d961f6ee43ad5e67e31f5e13878f3582ed841
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-10-30 13:20:06 +0300

    QuerySet запросы в базу данных

--- modul19/task1/models.py
+++ modul19/task1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10,decimal_places=10)
+    balance = models.DecimalField(max_digits=10000,decimal_places=10)
     age = models.IntegerField()
 
     def __str__(self):
@@ -12,8 +12,8 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=10,decimal_places=10)
-    size = models.DecimalField(max_digits=10,decimal_places=10)
+    cost = models.DecimalField(max_digits=10000,decimal_places=10)
+    size = models.DecimalField(max_digits=10000,decimal_places=10)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
